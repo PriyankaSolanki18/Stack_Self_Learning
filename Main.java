3ec893d2b53da00ea4202d26d1bc447d3fccc29f
priyankasolanki <priyankasolanki.1820@gmail.com>
2025-06-18 00:15:55 +0530

    Initial Commit (18-06-2025)

--- Main.java
+++ Main.java
@@ -4,16 +4,24 @@ public class Main {
     public static void main(String[] args) {
         Stack<Integer> st = new Stack<>();
         st.push(18);
+        System.out.println(st+" -> "+st.peek()+" "+st.size());
         st.push(34);
-        System.out.println(st);
+        System.out.println(st+" -> "+st.peek()+" "+st.size());
         st.push(6);
-        System.out.println(st);
+        System.out.println(st+" -> "+st.peek()+" "+st.size());
         st.push(78);
-        System.out.println(st);
         System.out.println(st+" -> "+st.peek()+" "+st.size());
         st.pop();
         System.out.println(st+" -> "+st.peek()+" "+st.size());
         st.pop();
         System.out.println(st+" -> "+st.peek()+" "+st.size());
+        st.pop();
+        System.out.println(st+" -> "+st.peek()+" "+st.size());
+        st.pop();
+        System.out.println(st+" -> "+st.size());
+
+        //Underflow Condition : No more elements are present to be removed from stack
+        //st.pop();
+        //System.out.println(st);
     }
 }
