4560a97a386a192b84ee83c12fe3b7165289e55d
priyankasolanki <priyankasolanki.1820@gmail.com>
2025-06-18 14:55:34 +0530

    Initial Commit (18-06-2025)

--- DuplicateBrackets.java
+++ DuplicateBrackets.java
@@ -1,14 +1,15 @@
+import javax.xml.transform.sax.SAXSource;
 import java.util.*;
 import java.io.*;
 
 public class DuplicateBrackets {
     public static void main(String[] args) throws Exception{
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter string : ");
+        System.out.print("Enter str : ");
         String str = sc.nextLine();
         Stack<Character> st = new Stack<>();
         for(int i=0;i<str.length();i++){
-            char ch=str.charAt(i);
+            char ch = str.charAt(i);
             if(ch == ')'){
                 if(st.peek() == '('){
                     System.out.println(true);
